900ff02401ea068d98d59f90921d5a27d40981ef
Huey <142527707+OfficerHuey@users.noreply.github.com>
2025-05-06 19:42:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -29,4 +29,6 @@ app.delete("/api/items/:id", async (req, res) => {
   res.sendStatus(204);
 });
 
-app.listen(5000, () => console.log("Backend running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
+
